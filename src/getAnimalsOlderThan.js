57f07f8292b3b9df58c1f0d5cf285137509ec9ac
carlatakagi <carlatakagi@gmail.com>
2022-01-06 16:18:35 -0400

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalSpecie = data.species.find((specie) => specie.name === animal);
+  return animalSpecie.residents.every((resident) => resident.age >= age);
 }
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
