d1f9924cfa8dc61177d021194914741e12de9bfb
carlatakagi <carlatakagi@gmail.com>
2022-01-06 16:44:05 -0400

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+
+  return data.employees.find((employee) => {
+    const employeeFirstName = employee.firstName;
+    const employeeLastName = employee.lastName;
+    return employeeFirstName === employeeName || employeeLastName === employeeName;
+  });
 }
 
 module.exports = getEmployeeByName;
