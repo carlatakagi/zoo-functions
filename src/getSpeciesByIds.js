a9d13078e8ca6bff213fd21344af483c1b78e123
carlatakagi <carlatakagi@gmail.com>
2022-01-06 15:42:37 -0400

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+// método includes https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return data.species.filter((specie) => ids.includes(specie.id));
 }
 
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
