9aaadd49ec0aee9ff755a34c1f5b3bc7c918fb9f
carlatakagi <carlatakagi@gmail.com>
2022-01-07 10:50:13 -0400

    requisito 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    return data.species.reduce((accumulator, specie) => {
+      // retornar todas as especies e sua quantidade
+    }, {});
+  }
 }
+console.log(countAnimals());
+// residents.length
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+/* const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managers = [stephanieId, olaId, burlId];
+ */
 function isManager(id) {
-  // seu código aqui
+  // seu código aqui -- encontrar UMA pessoa colaboradora gerente e retornar true, caso contratrio retornar false
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  const isManagerId = isManager(managerId);
+
+  if (isManagerId === true) {
+    return data.employees.reduce((accumulator, employee) => {
+      if (employee.managers.includes(managerId)) {
+        accumulator.push(`${employee.firstName} ${employee.lastName}`);
+      }
+      return accumulator;
+    }, []);
+  }
+  if (isManagerId === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
+
+console.log(getRelatedEmployees(stephanieId));
+// console.log(getRelatedEmployees(managers));
 
 module.exports = { isManager, getRelatedEmployees };
