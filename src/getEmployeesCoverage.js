1cdcb2fc7db5275159a33023e9bf633d23839fa9
carlatakagi <carlatakagi@gmail.com>
2022-01-11 09:27:58 -0400

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,49 @@
+/* eslint-disable max-lines-per-function */
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getLocations(responsibleFor) {
+  /* return data.species.filter((specie) => responsibleFor.includes(specie.id))
+    .map((specie) => specie.location); */
+  return data.species.reduce((listLocation, currentSpecies) => {
+    if (responsibleFor.includes(currentSpecies.id)) {
+      listLocation.push(currentSpecies.location);
+    }
+    return listLocation;
+  }, []);
+}
+
+function getName(responsibleFor) {
+  return data.species.reduce((listName, currentSpecies) => {
+    if (responsibleFor.includes(currentSpecies.id)) {
+      listName.push(currentSpecies.name);
+    }
+    return listName;
+  }, []);
+}
+
+function getEmployeesCoverage(options) {
+  if (!options) {
+    return data.employees.map((employee) => ({
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: getName(employee.responsibleFor),
+      locations: getLocations(employee.responsibleFor),
+    }));
+  }
+
+  const findEmployee = data.employees.find((employee) => employee.id === options.id
+    || employee.firstName === options.name || employee.lastName === options.name);
+
+  if (!findEmployee) {
+    throw new Error('Informações inválidas');
+  }
+
+  return {
+    id: findEmployee.id,
+    fullName: `${findEmployee.firstName} ${findEmployee.lastName}`,
+    species: getName(findEmployee.responsibleFor),
+    locations: getLocations(findEmployee.responsibleFor),
+  };
 }
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,11 +4,11 @@ function getOldestFromFirstSpecies(id) {
   const findEmployee = data.employees.find((employee) => id === employee.id);
   const responsibleFor = findEmployee.responsibleFor[0];
   const findResidents = data.species.find((species) => responsibleFor === species.id).residents;
-  const findOldestAge = findResidents.reduce((oldestAge, currentAge) => {
-    if (oldestAge.age > currentAge.age) {
-      return oldestAge;
+  const findOldestAge = findResidents.reduce((previousResident, currentResident) => {
+    if (previousResident.age > currentResident.age) {
+      return previousResident;
     }
-    return currentAge;
+    return currentResident;
   });
   // retornar valores do objeto https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/values
   return Object.values(findOldestAge);
