c7ed18e40a9d0ee4dfdde6759015bca296757cbb
carlatakagi <carlatakagi@gmail.com>
2022-01-16 21:37:19 -0400

    alterações

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,37 +1,51 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  /* const { sex } = options || {};
-
-  return data.species.reduce((speciesObj, specie) => {
-    const speciesObjCopy = speciesObj;
-
-    if (sex) {
-      console.log(sex);
-    }
-
-    if (!speciesObjCopy[specie.location]) {
-      speciesObjCopy[specie.location] = [specie.name];
+// funcao para pegar todas as loc e animais
+function getAllAnimalsAndLocations() {
+  return data.species.reduce((accumulator, currentSpecie) => {
+    const accumulatorCopy = accumulator;
+    const keyLocationExist = accumulatorCopy[currentSpecie.location];
+    if (keyLocationExist) {
+      accumulatorCopy[currentSpecie.location].push(currentSpecie.name);
     } else {
-      speciesObjCopy[specie.location].push(specie.name);
+      accumulatorCopy[currentSpecie.location] = [currentSpecie.name];
     }
-
-    return speciesObjCopy;
-  }, {}); */
+    return accumulatorCopy;
+  }, {});
 }
 
-console.log(getAnimalMap({ sex: 'female' }));
-/* const expected = {
-  NE: [
-    { lions: ['Zena', 'Maxwell', 'Faustino', 'Dee'] },
-    { giraffes: ['Gracia', 'Antone', 'Vicky', 'Clay', 'Arron', 'Bernard'] },
-  ],
-  location : [name, name]
-    NE: ['lions', 'giraffes'],
-    NW: ['tigers', 'bears', 'elephants'],
-    SE: ['penguins', 'otters'],
-    SW: ['frogs', 'snakes'],
-  };
-*/
+// funcao para pegar nomes dos residents
+function getResidentsNames(residents, sex) {
+  return residents.map((resident) => resident.name);
+
+  // if (sex) return;
+}
+
+// { lions: ['Zena', 'Maxwell', 'Faustino', 'Dee'] }
+// funcao para para nome dos animais em um array
+function getAllAnimalsResidentsAndLocations() {
+  return data.species.reduce((accumulator, currentSpecie) => {
+    const accumulatorCopy = accumulator;
+    const keyLocationExist = accumulatorCopy[currentSpecie.location];
+    if (keyLocationExist) {
+      accumulatorCopy[currentSpecie.location].push({
+        [currentSpecie.name]: getResidentsNames(currentSpecie.residents),
+      });
+    } else {
+      accumulatorCopy[currentSpecie.location] = [{
+        [currentSpecie.name]: getResidentsNames(currentSpecie.residents),
+      }];
+    }
+    return accumulatorCopy;
+  }, {});
+}
+
+// funcao principal
+function getAnimalMap(options) {
+  const hasOptionIncludeName = options && options.includeNames;
+  if (hasOptionIncludeName) return getAllAnimalsResidentsAndLocations();
+
+  return getAllAnimalsAndLocations();
+}
 
 module.exports = getAnimalMap;
