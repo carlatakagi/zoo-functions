7049c9985ef7e341bb61a10be92cc53c082f9dfe
carlatakagi <carlatakagi@gmail.com>
2022-01-17 10:15:06 -0400

    requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,5 +1,5 @@
 const data = require('../data/zoo_data');
-
+// minha irmã me ajudou
 // funcao para pegar todas as loc e animais
 function getAllAnimalsAndLocations() {
   return data.species.reduce((accumulator, currentSpecie) => {
@@ -15,25 +15,37 @@ function getAllAnimalsAndLocations() {
 }
 
 // funcao para pegar nomes dos residents
-function getResidentsNames(residents, sex) {
-  return residents.map((resident) => resident.name);
-
-  // if (sex) return;
+function getResidentsNames({ residents, sex, sorted }) {
+  return residents.reduce((accumulator, currentSpecie) => {
+    // parenteses para separar o que tem que fazer primeiro
+    if ((sex && sex === currentSpecie.sex) || !sex) {
+      accumulator.push(currentSpecie.name);
+    }
+    if (sorted) {
+      accumulator.sort();
+    }
+    return accumulator;
+  }, []);
 }
 
-// { lions: ['Zena', 'Maxwell', 'Faustino', 'Dee'] }
 // funcao para para nome dos animais em um array
-function getAllAnimalsResidentsAndLocations() {
+// params => { sex, sorted }
+// named parameters utiliza para organizar parametros, normalmente utilizado a partir de três parametros
+function getAllAnimalsResidentsAndLocations({ sex, sorted }) {
   return data.species.reduce((accumulator, currentSpecie) => {
     const accumulatorCopy = accumulator;
     const keyLocationExist = accumulatorCopy[currentSpecie.location];
     if (keyLocationExist) {
       accumulatorCopy[currentSpecie.location].push({
-        [currentSpecie.name]: getResidentsNames(currentSpecie.residents),
+        [currentSpecie.name]: getResidentsNames({
+          residents: currentSpecie.residents, sex, sorted,
+        }),
       });
     } else {
       accumulatorCopy[currentSpecie.location] = [{
-        [currentSpecie.name]: getResidentsNames(currentSpecie.residents),
+        [currentSpecie.name]: getResidentsNames({
+          residents: currentSpecie.residents, sex, sorted,
+        }),
       }];
     }
     return accumulatorCopy;
@@ -41,11 +53,16 @@ function getAllAnimalsResidentsAndLocations() {
 }
 
 // funcao principal
-function getAnimalMap(options) {
-  const hasOptionIncludeName = options && options.includeNames;
-  if (hasOptionIncludeName) return getAllAnimalsResidentsAndLocations();
+// default de funcao getAnimalMap(options = {})
+function getAnimalMap(options = {}) {
+  const { includeNames, sex, sorted } = options;
+
+  if (includeNames) return getAllAnimalsResidentsAndLocations({ sex, sorted });
 
   return getAllAnimalsAndLocations();
 }
+
+// getAllAnimalsResidentsAndLocations(sex, sorted) - standard parameters
+// getAllAnimalsResidentsAndLocations({ sorted, sex }) - named parameters /parametros nomeados
 
 module.exports = getAnimalMap;
